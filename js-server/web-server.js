7afe001a8814a2725b14bc55b05a50dc9e29c32a
sttiwahm <tina.wahmkow@student.hs-anhalt.de>
2021-08-27 13:54:26 +0200

    cors added

--- js-server/web-server.js
+++ js-server/web-server.js
@@ -3,10 +3,12 @@ const getAll = require('./functions/getAll');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+const cors = require('cors');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
