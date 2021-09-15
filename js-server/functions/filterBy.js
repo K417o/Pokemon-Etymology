c3c4167209b3fe0dc9f83dde097fe7335a53ab71
Wutras <wunsch.jonas@hotmail.com>
2021-09-15 20:47:10 +0200

    Add support for new shape modelling, name parts and external origin references

--- js-server/functions/filterBy.js
+++ js-server/functions/filterBy.js
@@ -30,35 +30,46 @@ module.exports = {
     pkmnName = pkmnName.toLowerCase();
 
     const spQuery = `
-          PREFIX owl:  <http://www.w3.org/2002/07/owl#>
-          PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-          PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-          PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-          PREFIX poke: <https://pokemonkg.org/ontology#>
-          PREFIX dbpedia:	<http://dbpedia.org/resource/>
-          PREFIX qudt: <http://qudt.org/schema/qudt/>
-          PREFIX pokeetym: <urn:pokeetymology:ontology#>
-
-
-          SELECT ?name ?type ?colour ?genus ?height ?weight ?origin ?pic ?shape WHERE  {
-            ?pkmn rdfs:label ?name ;
-                  pokeetym:inspiredBy ?origin;
-                  foaf:depiction ?pic;
-                  poke:hasGenus ?genus ;
-                  poke:hasType ?type ;
-                  poke:hasHeight ?qudtH ;
-                  poke:hasWeight ?qudtW ;
-                  poke:hasShape ?shape ;
-                  poke:hasColour ?colour . 
-            ?qudtH qudt:quantityValue ?valueH.
-            ?valueH qudt:value ?height .
-            ?qudtW qudt:quantityValue ?valueW.
-            ?valueW qudt:value ?weight .
-            
-            FILTER (lcase(str(?name)) = "${pkmnName}")
-            FILTER(lang(?name)='${config.language}')
-            FILTER(lang(?genus)='${config.language}')
-        }`;
+    PREFIX owl:  <http://www.w3.org/2002/07/owl#>
+    PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX poke: <https://pokemonkg.org/ontology#>
+    PREFIX dbpedia:	<http://dbpedia.org/resource/>
+    PREFIX qudt: <http://qudt.org/schema/qudt/>
+    PREFIX pokeetym: <urn:pokeetymology:ontology#>
+    PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX dcterms: <http://purl.org/dc/terms/>
+    PREFIX isolang: <https://iso639-3.sil.org/code/>
+    
+    
+    SELECT ?name ?type ?colour ?genus ?height ?weight ?origin ?pic ?shape ?shapeImage ?dexId ?namePartTitle ?namePartPosInName ?namePartLang ?namePartDescription WHERE  {
+        ?pkmn foaf:name ?name ;
+        pokeetym:inspiredBy ?origin;
+        pokeetym:hasDexID ?dexIdEnt ;
+        pokeetym:hasNamePart ?namePart ;
+        foaf:depiction ?pic;
+        poke:hasGenus ?genus ;
+        poke:hasType ?type ;
+        poke:hasHeight ?qudtH ;
+        poke:hasWeight ?qudtW ;
+        poke:hasShape ?shape ;
+        poke:hasColour ?colour . 
+        ?qudtH qudt:quantityValue ?valueH.
+        ?valueH qudt:value ?height .
+        ?qudtW qudt:quantityValue ?valueW.
+        ?valueW qudt:value ?weight .
+        ?namePart pokeetym:positionInName ?namePartPosInName ;
+          dc:language ?namePartLang ;
+          dc:title ?namePartTitle .
+        OPTIONAL { ?namePart dc:description ?namePartDescription . }
+        ?dexIdEnt rdfs:label ?dexId .
+        ?shape foaf:depiction ?shapeImage .
+        
+        FILTER (lcase(str(?name)) = "${pkmnName}")
+        FILTER(lang(?name)='${config.language}')
+        FILTER(lang(?genus)='${config.language}')
+    }`;
 
     return query
       .execute(
@@ -67,7 +78,7 @@ module.exports = {
         spQuery,
         "application/sparql-results+json",
         {
-          limit: 20000,
+          limit: 50000,
           reasoning: true,
           offset: 0,
         }
@@ -80,17 +91,33 @@ module.exports = {
           weight: data[0].weight.value,
           height: data[0].height.value,
           colour: data[0].colour.value,
+          pokedexId: data[0].dexId.value,
+          shape: {
+            kind: data[0].shape.value,
+            depiction: data[0].shapeImage.value,
+          },
+          nameParts: {},
         };
         let types = new Set();
         let origins = new Set();
+        let nameParts = new Set();
+        let languages = new Set();
 
         for (let result of data) {
           origins.add(result.origin.value);
           types.add(result.type.value.slice(40));
-
-          if (/pokewiki/.test(result.shape.value)) {
-            response.shape = result.shape.value;
-          }
+          nameParts.add(
+            JSON.stringify({
+              title: result.namePartTitle.value,
+              description:
+                result.namePartDescription != null
+                  ? result.namePartDescription.value
+                  : null,
+              positionInName: result.namePartPosInName.value,
+              language: result.namePartLang.value,
+            })
+          );
+          languages.add(result.namePartLang.value);
 
           if (/pokewiki/.test(result.pic.value)) {
             response.image = result.pic.value;
@@ -98,20 +125,53 @@ module.exports = {
         }
 
         response.types = Array.from(types);
+        const parsedNameParts = Array.from(nameParts).map((namePart) =>
+          JSON.parse(namePart)
+        );
+
+        // Group by language
+        Array.from(languages).forEach(
+          (lang) => (response.nameParts[lang.slice(30)] = [])
+        );
+        parsedNameParts.forEach((namePart) =>
+          response.nameParts[namePart.language.slice(30)].push(namePart)
+        );
+
+        // Sort by position in name within language groups
+        Array.from(languages).forEach((lang) =>
+          response.nameParts[lang.slice(30)].sort(
+            (p1, p2) => p1.positionInName - p2.positionInName
+          )
+        );
+
         response.origins = await Promise.all(
           Array.from(origins).map((origin) => {
-            const originQuery = `SELECT ?label ?description ?pic
-          WHERE
-          {
-            wd:${origin.slice(32)} rdfs:label ?label .
-            OPTIONAL { wd:${origin.slice(
-              32
-            )} schema:description ?description . }
-            OPTIONAL { wd:${origin.slice(32)} wdt:P18 ?pic . }
-            FILTER(LANG(?label) = "${
-              config.language
-            }" && LANG(?description) = "${config.language}")
-          }`;
+            const originQuery = `SELECT ?label ?description ?pic ?externalRef ?externalSource WHERE
+            {
+              wd:${origin.slice(32)} rdfs:label ?label .
+              {
+                SELECT ?externalRef ?externalSource WHERE
+                {
+                  wd:${origin.slice(32)} ?prop ?stmt .
+                  ?stmt ?stmtProp ?externalRef .
+                  BIND(IRI(CONCAT(str(wd:), STRAFTER(str(?stmtProp), str(psn:)))) as ?externalSource)
+                  ?externalSource wikibase:propertyType wikibase:ExternalId .
+                }
+                LIMIT 3
+              }
+              OPTIONAL { wd:${origin.slice(
+                32
+              )} schema:description ?description . }
+              {
+                SELECT ?pic WHERE {
+                OPTIONAL { wd:${origin.slice(32)} wdt:P18 ?pic . }
+                }
+                LIMIT 1
+              }
+              FILTER(LANG(?label) = "${
+                config.language
+              }" && LANG(?description) = "${config.language}") 
+            }`;
 
             return axios
               .get(
@@ -123,10 +183,15 @@ module.exports = {
                 label: wikiRes.data.results.bindings[0].label.value,
                 description: wikiRes.data.results.bindings[0].description.value,
                 image: wikiRes.data.results.bindings[0].pic.value,
+                externalRefs: wikiRes.data.results.bindings.map((dataSet) => ({
+                  externalURI: dataSet.externalRef.value,
+                  externalSourceEntity: dataSet.externalSource.value,
+                })),
                 entity: origin.slice(32),
                 entityURI: origin,
               }))
               .catch((_) => ({
+                err: _,
                 entity: origin.slice(32),
                 entityURI: origin,
               }));
@@ -179,7 +244,6 @@ module.exports = {
     }
 
     if (Array.isArray(reqTypes)) {
-      console.log(reqTypes);
       if (strict == null || strict === "false") {
         reqTypes.forEach((type, index) => {
           if (index === reqTypes.length - 1) {
@@ -304,54 +368,73 @@ module.exports = {
         });
 
         return Promise.all(
-          Array.from(reducedData)
-            .map((keyValuePair) => {
-              const dataSet = keyValuePair[1];
-              return dataSet.reduce(
-                (prev, cur, index) => {
-                  if (/pokewiki/.test(cur.shape.value)) {
-                    prev.shape = cur.shape.value;
-                  }
-                  if (/pokewiki/.test(cur.pic.value)) {
-                    prev.image = cur.pic.value;
-                  }
-
-                  prev.origins.add(cur.origin.value);
-                  prev.types.add(cur.type.value.slice(40));
-
-                  if (index === dataSet.length - 1) {
-                    prev.origins = Array.from(prev.origins);
-                    prev.types = Array.from(prev.types);
-                  }
-
-                  return prev;
-                },
-                {
-                  name: dataSet[0].name.value,
-                  genus: dataSet[0].genus.value,
-                  weight: dataSet[0].weight.value,
-                  height: dataSet[0].height.value,
-                  colour: dataSet[0].colour.value.slice(28),
-                  shape: undefined,
-                  image: undefined,
-                  origins: new Set(),
-                  types: new Set(),
+          Array.from(reducedData).map((keyValuePair) => {
+            const dataSet = keyValuePair[1];
+            return dataSet.reduce(
+              (prev, cur, index) => {
+                if (/pokewiki/.test(cur.shape.value)) {
+                  prev.shape = cur.shape.value;
                 }
-              );
-            })
+                if (/pokewiki/.test(cur.pic.value)) {
+                  prev.image = cur.pic.value;
+                }
+
+                prev.origins.add(cur.origin.value);
+                prev.types.add(cur.type.value.slice(40));
+
+                if (index === dataSet.length - 1) {
+                  prev.origins = Array.from(prev.origins);
+                  prev.types = Array.from(prev.types);
+                }
+
+                return prev;
+              },
+              {
+                name: dataSet[0].name.value,
+                genus: dataSet[0].genus.value,
+                weight: dataSet[0].weight.value,
+                height: dataSet[0].height.value,
+                colour: dataSet[0].colour.value.slice(28),
+                shape: undefined,
+                image: undefined,
+                origins: new Set(),
+                types: new Set(),
+              }
+            );
+          })
+          /*
+            Zu lange Ladezeiten bzw. Probleme mit Rate Limits von wikidata
             .map(async (pokeData) => {
               const resolvedOrigins = await Promise.all(
                 Array.from(pokeData.origins).map((origin) => {
-                  const originQuery = `SELECT ?pic ?label ?description
-            WHERE
-            {
-              wd:${origin.slice(32)} wdt:P18 ?pic ;
-                         schema:description ?description ;
-                         rdfs:label ?label .
-              FILTER(LANG(?label) = "${
-                config.language
-              }" && LANG(?description) = "${config.language}")
-            }`;
+                  const originQuery = `SELECT ?label ?description ?pic ?externalRef ?externalSource WHERE
+                  {
+                    wd:${origin.slice(32)} rdfs:label ?label .
+                    {
+                      SELECT ?externalRef ?externalSource WHERE
+                      {
+                        OPTIONAL { 
+                          wd:${origin.slice(32)} ?prop ?stmt .
+                          ?stmt ?stmtProp ?externalRef .
+                          BIND(IRI(CONCAT(str(wd:), STRAFTER(str(?stmtProp), str(psn:)))) as ?externalSource)
+                          ?externalSource wikibase:propertyType wikibase:ExternalId .
+                        }
+                      }
+                      LIMIT 3
+                    }
+                    OPTIONAL { wd:${origin.slice(
+                      32
+                    )} schema:description ?description . }
+                    {
+                      SELECT ?pic WHERE {
+                      OPTIONAL { wd:${origin.slice(32)} wdt:P18 ?pic . }
+                      }
+                      LIMIT 1
+                    }
+                    FILTER(LANG(?label) = "${
+                      config.language
+                    }" && LANG(?description) = "${config.language}") 
+                  }`;
 
                   return axios
                     .get(
@@ -364,6 +447,12 @@ module.exports = {
                       description:
                         wikiRes.data.results.bindings[0].description.value,
                       image: wikiRes.data.results.bindings[0].pic.value,
+                      externalRefs: wikiRes.data.results.bindings.map(
+                        (dataSet) => ({
+                          externalURI: dataSet.externalRef.value,
+                          externalSourceEntity: dataSet.externalSource.value,
+                        })
+                      ),
                       entity: origin.slice(32),
                       entityURI: origin,
                     }))
@@ -378,7 +467,7 @@ module.exports = {
                 ...pokeData,
                 origins: resolvedOrigins,
               };
-            })
+            })*/
         );
       });
 
